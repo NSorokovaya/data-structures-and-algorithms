58ddd212b89d2818d7f7eb219e7c46c7b4576d00
NSorokovaya <nadiaasorokovaya@gmail.com>
2024-12-02 16:04:03 +0100

    dfs

---
+++ patterns/11.Depth-FirstSearch(DFS).js
@@ -0,0 +1,47 @@
+// iterative
+function DFS(graph, start) {
+  let visited = new Set();
+  let stack = [start];
+
+  while (stack.length > 0) {
+    let current = stack.pop();
+
+    if (!visited.has(current)) {
+      visited.add(current);
+
+      for (let neighbor of graph[current]) {
+        if (!visited.has(neighbor)) {
+          stack.push(neighbor);
+        }
+      }
+    }
+  }
+}
+
+//recursion
+
+function DFS(graph, start) {
+  let visited = new Set();
+
+  function recursive(graph, node, visited) {
+    if (visited.has(node)) return;
+
+    visited.add(node);
+
+    for (let neighbor of graph[node]) {
+      recursive(graph, neighbor, visited);
+    }
+  }
+
+  recursive(graph, start, visited);
+}
+
+let graph = {
+  1: [2, 3],
+  2: [4],
+  3: [5],
+  4: [],
+  5: [],
+};
+
+DFS(graph, 1);
